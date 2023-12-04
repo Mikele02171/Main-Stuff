6fb7d5315fa96e212bbca438561450d536974811
Michael Le <58547145+Mikele02171@users.noreply.github.com>
2023-12-05 09:21:30 +1000

    Add files via upload

---
+++ CS50P Introduction to Programming with Python/Week 1/Problem Set 1/bank.py
@@ -0,0 +1,7 @@
+message = input("Greeting: ")
+if message == "Hello" or message == "hello" or message == "hello, there" or message == " Hello " or message == "Hello, Newman" or message == " ":
+    print("$0")
+elif message == "How you doing?":
+    print("$20")
+else:
+    print("$100")
