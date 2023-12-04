67654ca712b406920a1b3faf7183511aef4f3189
Michael Le <58547145+Mikele02171@users.noreply.github.com>
2023-12-05 04:56:34 +1000

    Add files via upload

---
+++ CS50P Introduction to Programming with Python/Week 1/Lecture including code/src1 (1)/src1/grade3.py
@@ -0,0 +1,14 @@
+# Demonstrates fewer comparisons
+
+score = int(input("Score: "))
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
